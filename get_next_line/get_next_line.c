643e29ffb50399b6c11fe48042f5f7440ef1cc1f
David Novak <dnovak@student.42prague.com>
2024-06-20 19:42:22 +0200

    Setting up the project and unit tester.

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 18:08:05 by dnovak            #+#    #+#             */
+/*   Updated: 2024/06/20 19:31:23 by dnovak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+// Returns a line read from a file descriptor (fd). Returns NULL if there is nothing
+// else to read, or an error occurred.
+char	*get_next_line(int fd)
+{
+	(void)fd;
+	return (NULL);
+}
