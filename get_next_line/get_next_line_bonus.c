62f88e50965e14085df430f09f40c3deaa273505
David Novak <dnovak@student.42prague.com>
2024-06-25 11:54:16 +0200

    Version 4 - Corrected #include header for bonus

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static void	free_all(t_buf **list, char **line)
 {
